7bff92f775586f219ee63e786dbde016198963eb
Angelo Araújo <angelo.aristides@ifc.edu.br>
2021-10-23 02:32:43 -0300

    mandando os exercícios que faltavam

---
+++ Exercício15.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Exercício15 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada= new Scanner(System.in);
+		
+		final int TAM = 3;
+		int idade[] = new int [TAM];
+		float altura[] = new float [TAM];
+		float soma = 0; 
+		
+		
+		
+		for (int i = 0; i < TAM; i++) {
+		System.out.print("Informe sua idade: ");
+		idade [i]= entrada.nextInt();
+		System.out.print("Informe sua altura: ");
+		altura [i] = entrada.nextFloat();
+		soma = soma + altura[i];
+			}
+		
+		float mediaaltura = (soma/TAM);
+		int qtdalunos = 0;
+		 
+		for (int i = 0; i < TAM; i++) {
+			if ((idade[i] > 13) && (altura[i] < mediaaltura)) {
+				qtdalunos = qtdalunos + 1;
+			}
+		}
+		
+		System.out.println("A quantidade de alunos com mais de 13 anos e altura inferior à média é: "+qtdalunos);
+		
+		entrada.close();
+	}
+
+}
---
+++ Exercício16.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercício16 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada=new Scanner(System.in);
+		
+		final int TAM = 5;
+		int valor[] = new int [TAM];
+		
+		for (int i = 0; i < TAM; i++) {
+		do {
+			System.out.println("Informe um valor de 0 a 20: ");
+			valor[i] = entrada.nextInt();
+	
+		} while ((valor[i] < 0) || (valor[i] > 20)); 
+		}
+		
+		String texto = "#";
+		
+		for (int i = 0; i < TAM; i++) {
+			System.out.println(valor[i] + ": " + texto.repeat(valor[i]));
+		}
+
+		entrada.close();
+
+	}
+
+}
